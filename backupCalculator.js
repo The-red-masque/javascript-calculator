6b3b2d905e2a11d033286f632099912eb2ee7e62
the-telltale-heart <chris.farlee@gmail.com>
2026-07-01 22:51:51 -0700

    Functional-ish but hard to read.

---
+++ backupCalculator.js
@@ -0,0 +1,138 @@
+let numbers = [];
+/* BONUS for me: Create the buttons iteratively. */
+let numpad = ["0", ".", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let operators = ["+", "-", "/", "*"];
+const result = numbers.reduce(getSum, 0);
+let n1,operator,n2,newNumber;
+const roundingPlace = 5;
+
+const inputContainer = document.createElement("SECTION");
+inputContainer.classList.add("input-container");
+
+numpad.forEach(item => console.log(item))
+operators.forEach(item => console.log(item))
+
+const digitDiv = document.createElement("DIV");
+  digitDiv.classList.add("digit-container");
+  const zeroBtn = document.createElement("BUTTON");
+    zeroBtn.textContent = "0";
+    zeroBtn.classList.add("number-button");
+  digitDiv.appendChild(zeroBtn);
+  const decimalBtn = document.createElement("BUTTON");
+    decimalBtn.textContent = ".";
+    decimalBtn.classList.add("number-button");
+  digitDiv.appendChild(decimalBtn);
+  const oneBtn = document.createElement("BUTTON");
+    oneBtn.textContent = "1";
+    oneBtn.classList.add("number-button");
+  digitDiv.appendChild(oneBtn);
+  const twoBtn = document.createElement("BUTTON");
+    twoBtn.textContent = "2";
+    twoBtn.classList.add("number-button");
+  digitDiv.appendChild(twoBtn);
+  const threeBtn = document.createElement("BUTTON");
+    threeBtn.textContent = "3";
+    threeBtn.classList.add("number-button");
+  digitDiv.appendChild(threeBtn);
+  const fourBtn = document.createElement("BUTTON");
+    fourBtn.textContent = "4";
+    fourBtn.classList.add("number-button");
+  digitDiv.appendChild(fourBtn);
+  const fiveBtn = document.createElement("BUTTON");
+    fiveBtn.textContent = "5";
+    fiveBtn.classList.add("number-button");
+  digitDiv.appendChild(fiveBtn);
+  const sixBtn = document.createElement("BUTTON");
+    sixBtn.textContent = "6";
+    sixBtn.classList.add("number-button");
+  digitDiv.appendChild(sixBtn);
+  const sevenBtn = document.createElement("BUTTON");
+    sevenBtn.textContent = "7";
+    sevenBtn.classList.add("number-button");
+  digitDiv.appendChild(sevenBtn);
+  const eightBtn = document.createElement("BUTTON");
+    eightBtn.textContent = "8";
+    eightBtn.classList.add("number-button");
+  digitDiv.appendChild(eightBtn);
+  const nineBtn = document.createElement("BUTTON");
+    nineBtn.textContent = "9";
+    nineBtn.classList.add("number-button");
+  digitDiv.appendChild(nineBtn);
+  inputContainer.appendChild(digitDiv);
+
+const operatorDiv = document.createElement("div");
+  operatorDiv.classList.add("operator-container")
+  const equalsBtn = document.createElement("BUTTON");
+    equalsBtn.textContent = "=";
+    equalsBtn.classList = "operator-button";
+  operatorDiv.appendChild(equalsBtn);
+  const plusBtn = document.createElement("BUTTON");
+    plusBtn.textContent = "+";
+    plusBtn.classList = "operator-button";
+  operatorDiv.appendChild(plusBtn);
+  const minusBtn = document.createElement("BUTTON");
+    minusBtn.textContent = "-";
+    minusBtn.classList = "operator-button";
+  operatorDiv.appendChild(minusBtn);
+  const divisionBtn = document.createElement("BUTTON");
+    divisionBtn.textContent = "/";
+    divisionBtn.classList = "operator-button";
+  operatorDiv.appendChild(divisionBtn);
+  const multiplyBtn = document.createElement("BUTTON");
+    multiplyBtn.textContent = "*";
+    multiplyBtn.classList = "operator-button";
+  operatorDiv.appendChild(multiplyBtn);
+inputContainer.appendChild(operatorDiv);
+document.body.appendChild(inputContainer);
+
+// Rounding logic
+const roundingLogic = (Math.round(newNumber*(10**roundingPlace)))/(10**roundingPlace);
+
+function add (n1,n2) {
+  newNumber = n1+n2;
+
+  numbers.push((Math.round(newNumber*(10**roundingPlace)))/(10**roundingPlace));
+  //numbers.push(newNumber);
+}
+function subtract (n1,n2) {
+  newNumber = n1-n2;
+  numbers.push((Math.round(newNumber*(10**roundingPlace)))/(10**roundingPlace));
+}
+function multiply (n1,n2) {
+  newNumber = n1 * n2;
+  numbers.push((Math.round(newNumber*(10**roundingPlace)))/(10**roundingPlace));
+}
+function divide   (n1,n2) {
+  newNumber = n1/n2;
+  numbers.push((Math.round(newNumber*(10**roundingPlace)))/(10**roundingPlace));
+}
+function clearNumbers ()  {numbers = [];}
+function operate  (n1,operator,n2) {
+  if (operator === "+") {
+    add(n1,n2);
+  }
+  else if (operator === "-"){
+    subtract(n1,n2);
+  }
+  else if (operator === "/") {
+    divide(n1,n2);
+  }
+  else if (operator === "*"){
+    multiply(n1,n2);
+  } else {alert("Whoops!")}
+}
+
+function getSum(total, num) {
+  return total + Math.round(num);
+}
+
+// document.getElementById("0key").addEventListener("click", function(){ n1? n1=0 : n2=0})  // Is there a better way to do this?
+// document.getElementById("1key").onclick = function(){ n1? n1=1 : n2=1};
+// document.getElementById("2key").onclick = function(){ n1? n1=2 : n2=2};
+// document.getElementById("3key").onclick = function(){ n1? n1=3 : n2=3};
+// document.getElementById("4key").onclick = function(){ n1? n1=4 : n2=4};
+// document.getElementById("5key").onclick = function(){ n1? n1=5 : n2=5};
+// document.getElementById("6key").onclick = function(){ n1? n1=6 : n2=6};
+// document.getElementById("7key").onclick = function(){ n1? n1=7 : n2=7};
+// document.getElementById("8key").onclick = function(){ n1? n1=8 : n2=8};
+// document.getElementById("9key").onclick = function(){ n1? console.log(n1=9) : console.log(n2=9)};
