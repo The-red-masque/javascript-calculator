8bca127a13dd420e0d1f5b823028beed2b3c086a
the-telltale-heart <chris.farlee@gmail.com>
2026-07-02 19:05:19 -0700

    Made button generation cleaner.

--- calculator.js
+++ calculator.js
@@ -3,33 +3,47 @@ let numbers = [];
 let numpad = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "C"];
 let operators = ["*", "/", "-", "+"];
 
+
 const result = numbers.reduce(getSum, 0);
 let n1,operator,n2,newNumber;
 const roundingPlace = 5;
-
+const displayContainer = document.createElement("SECTION");
+displayContainer.classList.add("display-container");
+const displayDiv = document.createElement("DIV");
+displayDiv.textContent="The answer is 42.";
+displayContainer.appendChild(displayDiv);
+document.body.appendChild(displayContainer);
 const inputContainer = document.createElement("SECTION");
 inputContainer.classList.add("input-container");
 
 const digitDiv = document.createElement("DIV");
   digitDiv.classList.add("digit-container");
   numpad.forEach(item => {
-    const btn = document.createElement("BUTTON");
-    btn.textContent=item;
-    btn.classList.add("number-button");
-    digitDiv.appendChild(btn);
+    createDigitBtn(item);
   });
   inputContainer.appendChild(digitDiv);
 
   const operatorDiv = document.createElement("DIV");
     operatorDiv.classList.add("operators-container");
     operators.forEach(item => {
-      const btn = document.createElement("BUTTON");
-      btn.textContent=item;
-      btn.classList.add("operator-button");
-    operatorDiv.appendChild(btn);
+      createOperatorBtn(item);
   });
   inputContainer.appendChild(operatorDiv);
 document.body.appendChild(inputContainer);
+
+function createDigitBtn(item) {
+  const btn = document.createElement("BUTTON");
+  btn.textContent=item;
+  btn.classList.add("number-button");
+  digitDiv.appendChild(btn);
+}
+
+function createOperatorBtn(item) {
+  const btn = document.createElement("BUTTON");
+  btn.textContent=item;
+  btn.classList.add("operator-button");
+  operatorDiv.appendChild(btn);
+}
 
 // Rounding logic
 const roundingLogic = (Math.round(newNumber*(10**roundingPlace)))/(10**roundingPlace);
